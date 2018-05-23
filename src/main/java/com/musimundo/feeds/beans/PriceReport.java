506c072fe484b1c21d6683cff7ec1a23fbe4734d
Gustav <gustavsanchez@yahoo.com.ar>
2018-05-23 19:07:41 -0300

    -se implementó model, dao, services y controller de todos los feeds

---
+++ src/main/java/com/musimundo/feeds/beans/PriceReport.java
@@ -0,0 +1,54 @@
+package com.musimundo.feeds.beans;
+
+public class PriceReport {
+
+    private Long countTotal, countOk, countWarning, countError, countNotProcessed;
+
+    public PriceReport() {
+        countTotal = 0L;
+        this.countOk = 0L;
+        this.countWarning = 0L;
+        this.countError = 0L;
+        this.countNotProcessed = 0L;
+    }
+
+    public Long getCountTotal() {
+        return countTotal;
+    }
+
+    public void setCountTotal(Long countTotal) {
+        this.countTotal = countTotal;
+    }
+
+    public Long getCountOk() {
+        return countOk;
+    }
+
+    public void setCountOk(Long countOk) {
+        this.countOk = countOk;
+    }
+
+    public Long getCountWarning() {
+        return countWarning;
+    }
+
+    public void setCountWarning(Long countWarning) {
+        this.countWarning = countWarning;
+    }
+
+    public Long getCountError() {
+        return countError;
+    }
+
+    public void setCountError(Long countError) {
+        this.countError = countError;
+    }
+
+    public Long getCountNotProcessed() {
+        return countNotProcessed;
+    }
+
+    public void setCountNotProcessed(Long countNotProcessed) {
+        this.countNotProcessed = countNotProcessed;
+    }
+}
